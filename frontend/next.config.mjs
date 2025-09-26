703a73b0fc827bd3a21ef03319222d13a460b9c7
Nitesh <niteshverma87409@gmail.com>
2025-09-26 12:27:29 +0530

    css error resolvedn for server side

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // Build ke time ESLint errors ignore kar dega
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    optimizeCss: false, // fonts aur CSS optimization ke liye
+  },
+};
 
 export default nextConfig;
